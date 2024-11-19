8d36150cfbceccc9ee39db206435fdffd77cf2e9
Scott Pei <peileiscott@gmail.com>
2024-11-19 16:33:09 +0800

    Update examples

--- main.go
+++ main.go
@@ -10,17 +10,20 @@ import (
 func main() {
 	r := gee.New()
 	r.GET("/", func(c *gee.Context) {
-		c.String(http.StatusOK, "Hello World!\n")
+		c.HTML(http.StatusOK, "<h1>Welcome to home page</h1>")
 	})
 
-	r.POST("/ping", func(c *gee.Context) {
-		c.JSON(http.StatusCreated, gee.H{
-			"message": "pong",
+	r.GET("/hello", func(c *gee.Context) {
+		// GET /hello?name=[name]
+		c.String(http.StatusOK, "Hello %s\n", c.Query("name"))
+	})
+
+	r.POST("/login", func(c *gee.Context) {
+		// POST /login username=[username]&password=[password]
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
 		})
-	})
-
-	r.GET("/html", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello World</h1>")
 	})
 
 	if err := r.Run(":9999"); err != nil {
